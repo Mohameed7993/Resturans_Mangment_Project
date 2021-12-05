f2ec4ec8fa38ac41edbbd9baaf54baa6253186ac
Mohameed7993 <hmody.chechareto@gmail.com>
2021-12-05 16:35:11 +0200

    removed line

--- Assignment3_Client/src/client/ClientUI.java
+++ Assignment3_Client/src/client/ClientUI.java
@@ -11,7 +11,7 @@ public class ClientUI extends Application {
 
 	public static void main( String args[] ) throws Exception
 	   { 
-// mohamed egbaria
+
 		    launch(args);  
 	   } // end main
 	 
